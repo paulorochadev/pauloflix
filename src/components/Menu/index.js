eadbac277ed41299c9b566274abd8e0ef72b6421
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2020-07-28 13:36:42 -0300

    Ajustes de navegação

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import Logo from '../../assets/img/Logo.png';
 import Button from '../Button';
 // import ButtonLink from './components/ButtonLink';
@@ -8,11 +10,11 @@ import './Menu.css';
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="PauloFlix Logo" />
-      </a>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
       </Button>
     </nav>
